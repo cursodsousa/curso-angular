4f8124ea2aa663e0c2679d3c12c82b9c5b43ef6b
dougllas.sousa <dougllas.sousa@seduc.ce.gov.br>
2025-01-12 17:50:53 -0300

    [crud-app] finalizando a aplicação simples

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -6,9 +6,11 @@ import { MatInputModule } from '@angular/material/input'
 import { FormsModule } from '@angular/forms'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatSnackBar } from '@angular/material/snack-bar'
 import { Cliente } from './cliente'
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask'
 
 @Component({
   selector: 'app-cadastro',
@@ -19,7 +21,10 @@ import { ActivatedRoute, Router } from '@angular/router';
     MatFormFieldModule, 
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    NgxMaskDirective,
+  ], providers: [
+    provideNgxMask()
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
@@ -28,6 +33,7 @@ export class CadastroComponent implements OnInit {
 
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
+  snack: MatSnackBar = inject(MatSnackBar)
 
   constructor(
     private service: ClienteService,
@@ -55,9 +61,15 @@ export class CadastroComponent implements OnInit {
     if(!this.atualizando){
       this.service.salvar(this.cliente); 
       this.cliente = Cliente.newCliente();   
+      this.mostrarMensagem('Salvo com sucesso!')
     } else {
       this.service.atualizar(this.cliente);
       this.router.navigate(['/consulta'])
+      this.mostrarMensagem('Atualizado com sucesso!')
     }
+  }
+
+  mostrarMensagem(mensagem: string){
+    this.snack.open(mensagem, "Ok")
   }
 }
